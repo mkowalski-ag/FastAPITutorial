a2e4467980c49b735b727b3f776b51d6c96a67a9
Malind Kowalski <malind@agsnm.com>
2024-11-26 22:39:03 -0700

    added POST route

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from fastapi import FastAPI
 # create a new app
 app = FastAPI()
 
+# create empty list to use for routes
+dogs = []
+
 # define a path in fastapi
 @app.get('/')
 def root():
     return ("Hello World from Python")
+
+@app.post('/dogs')
+def create_dog(dog: str): #query parameter
+    dogs.append(dog)
+    return dogs
